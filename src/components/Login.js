de5d9cb0e3f7942bcd4a9c0cdc4ba0fe7de831af
lim211 <lim211@mcmaster.ca>
2022-08-24 19:01:23 -0400

    Removed unnecessary OAuth scopes

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,7 @@
 import { Container } from "react-bootstrap";
 import { HOST_URL } from "../data";
 
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=cb09cd6fc8b14816adfcc582ec5698b2&response_type=code&redirect_uri=${HOST_URL}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=cb09cd6fc8b14816adfcc582ec5698b2&response_type=code&redirect_uri=${HOST_URL}&scope=streaming%20user-read-private%20user-library-read%20user-read-playback-state%20user-modify-playback-state`;
 
 export default function Login() {
   return (
